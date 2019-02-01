0e56a53ae384c2608f07cada68c5fa3077107151
yumetodo <yume-wikijp@live.jp>
2019-02-02 01:04:35 +0900

    fix(cvt): to_roaji: fix #1
    when parsing consonant after `n`, it was detected as no conversion

--- src/cvt.rs
+++ src/cvt.rs
@@ -32,14 +32,17 @@ impl RomajiCvt {
         let c1 = it.next()?;
         let cnt = it.take_while(|c| c1 == *c).count();
         if 'n' == c1 {
-            let mut real_cnt = (cnt + 1) / 2;
-            if 0 == real_cnt && '\'' == s.chars().nth(1)? {
-                real_cnt = 1;
-            }
-            if 0 == real_cnt {
-                None
+            let kana_cnt = (cnt + 1) / 2;
+            if 0 == kana_cnt {
+                if '\'' == s.chars().nth(1)? {
+                    Some(('ん'.to_string(), 2))
+                } else if self.is_consonant(s.chars().nth(1)?) {
+                    Some(('ん'.to_string(), 1))
+                } else {
+                    None
+                }
             } else {
-                Some((iter::repeat('ん').take(real_cnt).collect::<String>(), real_cnt * 2))
+                Some((iter::repeat('ん').take(kana_cnt).collect::<String>(), kana_cnt * 2))
             }
         } else if 0 == cnt {
             None
@@ -142,6 +145,7 @@ mod test {
         assert_eq!(Some("むっ".to_string()), cvt.from_romaji("muxtu".to_string()));
         assert_eq!(Some("くっっころ".to_string()), cvt.from_romaji("kukkkoro".to_string()));
         assert_eq!(Some("さんばし".to_string()), cvt.from_romaji("san'basi".to_string()));
+        assert_eq!(Some("なんでやねん".to_string()), cvt.from_romaji("nandeyanen".to_string()));
     }
     #[test]
     fn to_romaji() {
